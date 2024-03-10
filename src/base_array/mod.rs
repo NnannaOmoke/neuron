f91f7b540104d40e39c3e4c2bb58a28aa63adbfb
Nnanna <omokennanna832@gmail.com>
2024-03-11 00:06:59 +0100

    Work on the base data structures

---
+++ src/base_array/mod.rs
@@ -0,0 +1,59 @@
+use crate::*;
+//we might need to define types for the dataset. each column can only have one type associated with it
+pub(crate) type float = f32;
+pub(crate) type int = i32;
+pub(crate) type unsigned_int = u32;
+pub(crate) type double = f64;
+pub(crate) type long = i64;
+pub(crate) type long_long = i128;
+//we'll have to so something about strings
+//if we have a numerical value that we can't parse, assume that the whole column is a string col
+pub(crate) type object = String;
+
+//These are the base types that each column must possess
+//to use them, we can take the types we have, alias them, and impl `BaseType` for them
+
+impl BaseType<float> for float{}
+impl BaseType<int> for int{}
+impl BaseType<unsigned_int> for unsigned_int{}
+impl BaseType<double> for double{}
+impl BaseType<long> for long{}
+impl BaseType<long_long> for long_long{}
+
+
+pub(crate) trait BaseType<T>
+where T: Sized + Copy {}
+
+#[repr(C)]
+pub(crate) struct BaseMatrix<A: BaseType<A> + Copy>{
+    data: Array2<A>
+}
+
+impl<A: BaseType<A> + Copy> BaseMatrix<A>{
+    //@Sporadic Creator you're going to have to save me on this one :(
+    // pub(crate) fn from_csv(sep: &str, fname: PathBuf) -> Self{
+        
+    // }
+    pub(crate) fn transpose(self) -> Self{
+        BaseMatrix{data: self.data.reversed_axes()}
+    }
+    pub(crate) fn shape(&self) -> (usize, usize){
+        assert!(self.data.shape().len() == 2);
+        (self.data.nrows(), self.data.ncols())
+    }
+    pub(crate) fn get_col(&self, cindex: usize) -> &[A]{
+        self.data.column(cindex).to_slice().unwrap()
+    }
+    pub(crate) fn get_row(&self, rindex: usize) -> &[A]{
+        self.data.row(rindex).to_slice().unwrap()
+    }
+    pub(crate) fn get(&self, rindex: usize, cindex: usize) -> A{
+        *self.data.get((rindex, cindex)).unwrap()
+    }
+}
+//here are the tasks: i.e. @sporadic_creator, please implement two iterators, one that will yield the cols, and the other the rows
+//i.e. a row iterator that yields a tuple containing the data in a row and a column iterator that yields the data in a column
+
+//@ViableCompute, I want you to implement std::ops::traits for BaseMatrix [add, sub, mult(dot and element wise), div, index(use the get() function)]. When we're done with that we'll write a more userfriendly API that will
+//be visible for our users, similar to pandas dataframe
+
---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+//All imports are defined here and made (pub)crate
+pub(crate) use std::{
+    fs::OpenOptions, 
+    path::PathBuf,
+    io::BufReader,
+};
+pub(crate) use ndarray::{Array2, Ix2};
+use nalgebra;
+
+// fn test_something(){
+//     let arr = nalgebra::DMatrix::repeat(100, 100, 4f32);
+//     let arr_two = nalgebra::DMatrix::repeat(100, 100, 4f32);
+//     let mut alloc: nalgebra::Matrix<f32, nalgebra::Dyn, nalgebra::Dyn, nalgebra::VecStorage<_, nalgebra::Dyn, nalgebra::Dyn>> = nalgebra::DMatrix::identity(100, 100);
+//     alloc.gemm(1f32, &arr, &arr_two, 0f32);
+// }
+
+
+//we will use this to bench the performance of seperate 2d matrices with criterion
+//instead of pub(crate)-ing criterion, since we won't use it elsewhere we'll just import it privately
+mod base_array;
+
