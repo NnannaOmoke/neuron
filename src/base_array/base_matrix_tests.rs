3a446b1594c6185d528da3f7af9e60b29048262e
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-03-25 09:45:28 -0700

    Should fix issue of all fields being parsed as objects.

--- src/base_array/base_matrix_tests.rs
+++ src/base_array/base_matrix_tests.rs
@@ -1,11 +1,11 @@
 use super::{base_dataset::BaseDataset, BaseMatrix};
 use std::{borrow::Cow, path::Path};
 
+static CSV1: &'static str = include_str!("test_data/test.csv");
+
 #[test]
 pub fn test_base_matrix_try_from_csv() {
-    static DATA: &'static str = include_str!("test_data/test.csv");
-
-    let csv_reader = csv::ReaderBuilder::new().from_reader(DATA.as_bytes());
+    let csv_reader = csv::ReaderBuilder::new().from_reader(CSV1.as_bytes());
     let data = BaseDataset::try_from_csv_reader(
         csv_reader,
         false,
