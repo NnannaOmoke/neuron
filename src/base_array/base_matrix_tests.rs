2a7f78173567d7bd1f061982a3987b16cb365c25
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-04-09 08:34:42 -0700

    Add a couple tests to BaseMatrix.

--- src/base_array/base_matrix_tests.rs
+++ src/base_array/base_matrix_tests.rs
@@ -1,17 +1,65 @@
-use crate::dtype::DType;
 use super::{base_dataset::BaseDataset, BaseMatrix};
-use std::{borrow::Cow, path::Path};
+use crate::dtype::DType;
+use csv::Reader;
+use ndarray::{arr2, Array2};
+use std::{borrow::Cow, fs::File, path::Path};
 
 #[test]
 pub fn test_base_matrix_try_from_csv() {
-    let data = BaseDataset::from_csv(
-        Path::new("src/base_array/test_data/test.csv"),
+    let data = BaseMatrix::try_from_csv(
+        Reader::from_path("src/base_array/test_data/test.csv").unwrap(),
         false,
-        false,
-        b',',
+    )
+    .unwrap();
+    // TODO: Change if DType is changed to prioritze integer parsing.
+    assert_eq!(
+        data.data,
+        arr2(&[
+            [
+                DType::Object("adamu".to_string()),
+                DType::F32(16.0),
+                DType::Object("male".to_string()),
+                DType::F32(86.4)
+            ],
+            [
+                DType::Object("james".to_string()),
+                DType::F32(19.0),
+                DType::Object("female".to_string()),
+                DType::F32(74.0)
+            ],
+            [
+                DType::Object("nissa".to_string()),
+                DType::F32(6.0),
+                DType::Object("female".to_string()),
+                DType::F32(15.0)
+            ],
+            [
+                DType::Object("karkarot".to_string()),
+                DType::F32(893.0),
+                DType::Object("saiyan".to_string()),
+                DType::F32(100.0)
+            ]
+        ])
     );
-    let data = data.unwrap();
-    data.head(None);
-    println!("{:?}", data.columns());
-    println!("{}", data.len());
+}
+
+#[test]
+fn test_transpose() {
+    let inner_data = Array2::from_shape_fn((2, 2), |(i, j)| DType::U32(i as u32 + j as u32 * 2));
+    assert_eq!(
+        inner_data,
+        arr2(&[
+            [DType::U32(0), DType::U32(2)],
+            [DType::U32(1), DType::U32(3)],
+        ])
+    );
+    let mut data = BaseMatrix { data: inner_data };
+    data = data.transpose();
+    assert_eq!(
+        data.data,
+        arr2(&[
+            [DType::U32(0), DType::U32(1)],
+            [DType::U32(2), DType::U32(3)],
+        ])
+    );
 }
