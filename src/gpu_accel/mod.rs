d8583b58830cafda2751f3dfe4e26736192ec241
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-06-01 11:39:24 -0700

    Redo GpuContext architecture.
    This only defines the basic structure and adds an instantiator so far but it's good to get this progress pushed for now in case we need to go back.

--- src/gpu_accel/mod.rs
+++ src/gpu_accel/mod.rs
@@ -1,10 +1,16 @@
-use std::{
-    borrow::Cow,
-    path::Path,
-    sync::{Arc, Mutex, OnceLock, Weak},
-};
+use log::{debug, error, info, trace, warn};
+#[cfg(not(feature = "async_impls"))]
+use std::sync::Mutex;
+use std::{borrow::Cow, fmt::Debug, path::Path, sync::Arc};
 use thiserror::Error;
+#[cfg(feature = "async_impls")]
+use tokio::sync::Mutex;
 use vulkano::{
+    buffer::Buffer,
+    command_buffer::allocator::{
+        StandardCommandBufferAlloc, StandardCommandBufferAllocator,
+        StandardCommandBufferAllocatorCreateInfo,
+    },
     device::{
         physical::{PhysicalDevice, PhysicalDeviceType},
         Device, DeviceCreateInfo, Queue, QueueCreateInfo, QueueFlags,
@@ -14,115 +20,113 @@ use vulkano::{
     memory::allocator::StandardMemoryAllocator,
 };
 
-/// The global gpu context.
-///
-/// Prefer access and initialization with [`get_global_gpu_context`].
-/// You may also prefer to initialize directly to force the usage of the default or using
-/// [`GpuContext::new`].
-pub static GLOBAL_GPU_CONTEXT: OnceLock<GpuContext> = OnceLock::new();
-
-/// Gets the global gpu context. Returns err if it had to init and that failed.
-pub fn get_global_gpu_context() -> Result<&'static GpuContext, Error> {
-    if let Some(ctx) = GLOBAL_GPU_CONTEXT.get() {
-        Ok(ctx)
-    } else {
-        let new_ctx = GpuContext::new()?;
-        Ok(GLOBAL_GPU_CONTEXT.get_or_init(|| new_ctx))
-    }
-}
-
-#[derive(Debug)]
 pub struct GpuContext {
     device: Arc<Device>,
-    queues: Vec<Arc<Queue>>,
-    // TODO: Maybe look into if other allocators are better suited?
-    allocator: Arc<StandardMemoryAllocator>,
+    queue_family_index: u32,
+    queues: Box<[Arc<Queue>]>,
+    next_queue: Arc<Mutex<usize>>,
+    memory_allocator: StandardMemoryAllocator,
+    command_buffer_allocator: StandardCommandBufferAllocator,
+    // Likely going to change this method of storage or remove it.
+    buffers: Vec<Arc<Buffer>>,
 }
 
 impl GpuContext {
-    pub fn init(
-        device: Arc<Device>,
-        queues: Vec<Arc<Queue>>,
-        allocator: Arc<StandardMemoryAllocator>,
-    ) -> GpuContext {
-        GpuContext {
-            device,
-            queues,
-            allocator,
-        }
-    }
+    pub fn new(
+        device_preference: DeviceSelector<
+            impl FnOnce(&mut dyn Iterator<Item = Arc<PhysicalDevice>>) -> (Arc<PhysicalDevice>, u32),
+        >,
+        queue_family_preference: QueueFamilySelector,
+    ) -> Result<GpuContext, Error> {
+        let library = VulkanLibrary::new()?;
+        let instance = Instance::new(library, InstanceCreateInfo::default())?;
+        trace!("Created Vulkan library and instance.");
 
-    pub fn new() -> Result<GpuContext, Error> {
-        let lib = VulkanLibrary::new()?;
-        let instance = Instance::new(lib, InstanceCreateInfo::default())?;
-
-        let physical_devices = instance.enumerate_physical_devices()?;
-        if physical_devices.len() == 0 {
+        let vulkan_devices = instance.enumerate_physical_devices()?;
+        if vulkan_devices.len() == 0 {
             return Err(Error::NoVulkanDevices);
         }
-
-        // Looking for discrete gpu as that will likely be more powerful.
-        let mut physical_device_tmp = None;
-        let mut queue_family_index_tmp = None;
-        for pd in physical_devices {
-            let maybe_queue_family_index = pd
-                .queue_family_properties()
-                .iter()
-                .position(|queue_family| queue_family.queue_flags.contains(QueueFlags::COMPUTE))
-                .map(|i| i as u32);
-            if let Some(queue_family_index) = maybe_queue_family_index {
-                let device_type = pd.properties().device_type;
-                if device_type == PhysicalDeviceType::DiscreteGpu {
-                    physical_device_tmp = Some(pd);
-                    queue_family_index_tmp = Some(queue_family_index);
-                    break;
-                } else if device_type as i32
-                    > physical_device_tmp
-                        .as_ref()
-                        .map_or(-1, |fbpd: &Arc<PhysicalDevice>| {
-                            fbpd.properties().device_type as i32
-                        })
-                {
-                    physical_device_tmp = Some(pd);
-                    queue_family_index_tmp = Some(queue_family_index)
-                }
-            }
-        }
-
-        debug_assert_eq!(
-            physical_device_tmp.is_some() || queue_family_index_tmp.is_some(),
-            physical_device_tmp.is_some() && queue_family_index_tmp.is_some()
-        );
-        let (physical_device, queue_family_index) =
-            if let (Some(pd), Some(qfi)) = (physical_device_tmp, queue_family_index_tmp) {
-                (pd, qfi)
-            } else {
-                return Err(Error::NoVulkanComputingDevices);
-            };
+        let (physical_device, queue_family_index) = select_device_and_queue_family(
+            vulkan_devices,
+            device_preference,
+            queue_family_preference,
+        )?;
+        info!("Selected physical device {physical_device:?} and its queue family at index {queue_family_index}.");
 
         let (device, queues_iter) = Device::new(
             physical_device,
             DeviceCreateInfo {
                 queue_create_infos: vec![QueueCreateInfo {
-                    queue_family_index,
+                    queue_family_index: queue_family_index,
                     ..Default::default()
                 }],
                 ..Default::default()
             },
         )?;
+        trace!("Created device.");
+        let queues = queues_iter.collect::<Vec<_>>().into_boxed_slice();
 
-        let allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+        let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+        let command_buffer_allocator = StandardCommandBufferAllocator::new(
+            device.clone(),
+            StandardCommandBufferAllocatorCreateInfo::default(),
+        );
 
-        Ok(GpuContext {
+        Ok(Self {
             device,
-            queues: queues_iter.collect(),
-            allocator,
+            queue_family_index,
+            queues,
+            next_queue: Arc::new(Mutex::new(0)),
+            memory_allocator,
+            command_buffer_allocator,
+            buffers: Vec::new(),
         })
     }
 }
 
+impl Debug for GpuContext {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("GpuContext")
+            .field("device", &self.device)
+            .field("queue_family_index", &self.queue_family_index)
+            .field("queues", &self.queues)
+            .field("next_queue", &self.next_queue)
+            .field("memory_allocator", &self.memory_allocator)
+            .field("command_buffer_allocator", &"_")
+            .finish()
+    }
+}
+
+#[derive(Default, PartialEq)]
+pub enum DeviceSelector<F>
+where
+    // I hate this so much but I honestly don't know if collecting them into a vec and passing that
+    // would be better.
+    F: FnOnce(&mut dyn Iterator<Item = Arc<PhysicalDevice>>) -> (Arc<PhysicalDevice>, u32),
+{
+    #[default]
+    HighPower,
+    LowPower,
+    Custom(F),
+}
+
+#[derive(Debug, Default)]
+pub enum QueueFamilySelector {
+    // This assumes that the implementation will put its preferred queue families first.
+    #[default]
+    First,
+    MostQueues,
+}
+
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("device selector condition failed")]
+    DeviceSelectorFailed,
+    #[error("{index} is not an index for a valid queue family for physical device {device:?}")]
+    InvalidQueueFamilyIndex {
+        index: u32,
+        device: Arc<PhysicalDevice>,
+    },
     #[error(transparent)]
     LibraryLoading(#[from] vulkano::library::LoadingError),
     #[error("no physical devices that support Vulkan also support computing")]
@@ -135,13 +139,85 @@ pub enum Error {
     Vulkan(#[from] vulkano::VulkanError),
 }
 
-fn device_type_prio(input: PhysicalDeviceType) -> i32 {
-    match input {
-        PhysicalDeviceType::Other => 0,
-        PhysicalDeviceType::Cpu => 1,
-        PhysicalDeviceType::VirtualGpu => 2,
-        PhysicalDeviceType::IntegratedGpu => 3,
-        PhysicalDeviceType::DiscreteGpu => 4,
-        _ => unreachable!(),
+fn select_device_and_queue_family(
+    physical_devices: impl Iterator<Item = Arc<PhysicalDevice>>,
+    device_selector: DeviceSelector<
+        impl FnOnce(&mut dyn Iterator<Item = Arc<PhysicalDevice>>) -> (Arc<PhysicalDevice>, u32),
+    >,
+    queue_family_selector: QueueFamilySelector,
+) -> Result<(Arc<PhysicalDevice>, u32), Error> {
+    match device_selector {
+        DeviceSelector::Custom(f) => {
+            let mut capture = physical_devices;
+            let res = f(&mut capture);
+            if res.1 >= res.0.queue_family_properties().len() as u32 {
+                Err(Error::InvalidQueueFamilyIndex {
+                    index: res.1,
+                    device: res.0,
+                })
+            } else {
+                Ok(res)
+            }
+        }
+        // This is probably a crap way of doing this but that can come back later.
+        _ => {
+            let device_qfi_iter = physical_devices.filter_map(|pd| {
+                let mut queue_family_iter = pd.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, qfp)| {
+                        let res = qfp.queue_flags.contains(QueueFlags::COMPUTE);
+                        if res {
+                            trace!("Found computing queue family ({qfp:?} at idx {i}) for device {pd:?}.")
+                        } else {
+                            trace!("Found queue family ({qfp:?} at idx {i}) for device {pd:?} but it did not
+                                support computing workloads.");
+                        }
+                        res
+                    });
+                let res = match &queue_family_selector {
+                    QueueFamilySelector::First => queue_family_iter.next(),
+                    QueueFamilySelector::MostQueues => {
+                        queue_family_iter.max_by_key(|(_, qfp)| qfp.queue_count)
+                    }
+                }
+                .map(|(qfi, _)| (pd.clone(), qfi as u32));
+                if let Some((pd, qfi)) = res.as_ref() {
+                    debug!("Found compute-capable device ({pd:?}) with nominated QFI {qfi}");
+                } else {
+                    debug!("Found device ({pd:?}) but it did not have any queue families that supported \
+                        compute workloads.");
+                }
+                res
+            });
+
+            match device_selector {
+                DeviceSelector::HighPower => {
+                    device_qfi_iter.min_by_key(|(pd, _)| match pd.properties().device_type {
+                        PhysicalDeviceType::DiscreteGpu => 0,
+                        PhysicalDeviceType::IntegratedGpu => 1,
+                        PhysicalDeviceType::VirtualGpu => 2,
+                        PhysicalDeviceType::Cpu => 3,
+                        PhysicalDeviceType::Other => 4,
+                        _ => 5,
+                    })
+                }
+                DeviceSelector::LowPower => {
+                    device_qfi_iter.min_by_key(|(pd, _)| match pd.properties().device_type {
+                        // TODO: These are somewhat arbitrary and should likely be revised.
+                        // The idea currently is to prioritize power saving while still computing efficiently
+                        // via gpu compute. Idk, might remove.
+                        PhysicalDeviceType::IntegratedGpu => 0,
+                        PhysicalDeviceType::VirtualGpu => 1,
+                        PhysicalDeviceType::DiscreteGpu => 2,
+                        PhysicalDeviceType::Cpu => 3,
+                        PhysicalDeviceType::Other => 4,
+                        _ => 5,
+                    })
+                }
+                DeviceSelector::Custom(_) => unreachable!(),
+            }
+            .ok_or(Error::NoVulkanComputingDevices)
+        }
     }
 }
