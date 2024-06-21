530ff6a8ef388d7b2c60bc54208b9fb897fe4379
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-06-21 13:02:19 -0700

    Primitive GpuContext struct, creation apparataces, and tests for new GpuContext::new.
    The GpuContext struct will likely need to change a lot and gain more fields to match the needs of the computing context.

--- src/gpu_accel/compute.rs
+++ src/gpu_accel/compute.rs
@@ -1,72 +1,72 @@
-/// TODO: Add docs.
-use super::{context::GpuContext, Error};
+// /// TODO: Add docs.
+// use super::{context::GpuContext, Error};
 
-use std::{ops::Deref, sync::Arc};
-use vulkano::{buffer::Buffer, device::DeviceOwned};
+// use std::{ops::Deref, sync::Arc};
+// use vulkano::{buffer::Buffer, device::DeviceOwned};
 
-pub struct ComputeContext<GpuContextPtr: Deref<Target = GpuContext>> {
-    gpu_context: GpuContextPtr,
-    input_buffers: Vec<Arc<Buffer>>,
-    output_buffers: Vec<Arc<Buffer>>,
-    support_buffers: Option<Vec<Arc<Buffer>>>,
-}
+// pub struct ComputeContext<GpuContextPtr: Deref<Target = GpuContext>> {
+//     gpu_context: GpuContextPtr,
+//     input_buffers: Vec<Arc<Buffer>>,
+//     output_buffers: Vec<Arc<Buffer>>,
+//     support_buffers: Option<Vec<Arc<Buffer>>>,
+// }
 
-impl<GpuContextPtr: Deref<Target = GpuContext>> ComputeContext<GpuContextPtr> {
-    pub fn from_raw_parts(parts: ComputeContextParts<GpuContextPtr>) -> Result<Self, Error> {
-        for buffer in parts.input_buffers.iter() {
-            if buffer.device() != parts.gpu_context.device() {
-                return Err(Error::WrongDevice {
-                    expected_device: parts.gpu_context.device().clone(),
-                    got_device: buffer.device().clone(),
-                    // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
-                    offending_argument: Some("output_buffers"),
-                });
-            }
-        }
-        for buffer in parts.output_buffers.iter() {
-            if buffer.device() != parts.gpu_context.device() {
-                return Err(Error::WrongDevice {
-                    expected_device: parts.gpu_context.device().clone(),
-                    got_device: buffer.device().clone(),
-                    // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
-                    offending_argument: Some("output_buffers"),
-                });
-            }
-        }
-        for buffer in parts.support_buffers.iter().flatten() {
-            if buffer.device() != parts.gpu_context.device() {
-                return Err(Error::WrongDevice {
-                    expected_device: parts.gpu_context.device().clone(),
-                    got_device: buffer.device().clone(),
-                    // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
-                    offending_argument: Some("support_buffers"),
-                });
-            }
-        }
+// impl<GpuContextPtr: Deref<Target = GpuContext>> ComputeContext<GpuContextPtr> {
+//     pub fn from_raw_parts(parts: ComputeContextParts<GpuContextPtr>) -> Result<Self, Error> {
+//         for buffer in parts.input_buffers.iter() {
+//             if buffer.device() != parts.gpu_context.device() {
+//                 return Err(Error::WrongDevice {
+//                     expected_device: parts.gpu_context.device().clone(),
+//                     got_device: buffer.device().clone(),
+//                     // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
+//                     offending_argument: Some("output_buffers"),
+//                 });
+//             }
+//         }
+//         for buffer in parts.output_buffers.iter() {
+//             if buffer.device() != parts.gpu_context.device() {
+//                 return Err(Error::WrongDevice {
+//                     expected_device: parts.gpu_context.device().clone(),
+//                     got_device: buffer.device().clone(),
+//                     // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
+//                     offending_argument: Some("output_buffers"),
+//                 });
+//             }
+//         }
+//         for buffer in parts.support_buffers.iter().flatten() {
+//             if buffer.device() != parts.gpu_context.device() {
+//                 return Err(Error::WrongDevice {
+//                     expected_device: parts.gpu_context.device().clone(),
+//                     got_device: buffer.device().clone(),
+//                     // TODO: Make the offending_argument thing take a string so we can do support_buffers[i]
+//                     offending_argument: Some("support_buffers"),
+//                 });
+//             }
+//         }
 
-        Ok(Self::from_raw_parts_unchecked(parts))
-    }
+//         Ok(Self::from_raw_parts_unchecked(parts))
+//     }
 
-    pub fn from_raw_parts_unchecked(
-        ComputeContextParts {
-            gpu_context,
-            input_buffers,
-            output_buffers,
-            support_buffers,
-        }: ComputeContextParts<GpuContextPtr>,
-    ) -> Self {
-        ComputeContext {
-            gpu_context,
-            input_buffers,
-            output_buffers,
-            support_buffers,
-        }
-    }
-}
+//     pub fn from_raw_parts_unchecked(
+//         ComputeContextParts {
+//             gpu_context,
+//             input_buffers,
+//             output_buffers,
+//             support_buffers,
+//         }: ComputeContextParts<GpuContextPtr>,
+//     ) -> Self {
+//         ComputeContext {
+//             gpu_context,
+//             input_buffers,
+//             output_buffers,
+//             support_buffers,
+//         }
+//     }
+// }
 
-pub struct ComputeContextParts<GpuContextPtr: Deref<Target = GpuContext>> {
-    pub gpu_context: GpuContextPtr,
-    pub input_buffers: Vec<Arc<Buffer>>,
-    pub output_buffers: Vec<Arc<Buffer>>,
-    pub support_buffers: Option<Vec<Arc<Buffer>>>,
-}
+// pub struct ComputeContextParts<GpuContextPtr: Deref<Target = GpuContext>> {
+//     pub gpu_context: GpuContextPtr,
+//     pub input_buffers: Vec<Arc<Buffer>>,
+//     pub output_buffers: Vec<Arc<Buffer>>,
+//     pub support_buffers: Option<Vec<Arc<Buffer>>>,
+// }
--- src/gpu_accel/mod.rs
+++ src/gpu_accel/mod.rs
@@ -2,37 +2,36 @@ pub mod compute;
 pub mod context;
 
 use std::{fmt::Debug, sync::Arc};
-use thiserror::Error;
-use vulkano::device::{physical::PhysicalDevice, Device};
+// use thiserror::Error;
 
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("device selector condition failed")]
-    DeviceSelectorFailed,
-    #[error(
-        "{index} is not an index for a valid queue family for physical device {physical_device:?}"
-    )]
-    InvalidQueueFamilyIndexPhysicalDevice {
-        index: u32,
-        physical_device: Arc<PhysicalDevice>,
-    },
-    #[error("{index} is not an index for a valid queue family for vulkano device {device:?}")]
-    InvalidQueueFamilyIndexDevice { index: u32, device: Arc<Device> },
-    #[error(transparent)]
-    LibraryLoading(#[from] vulkano::library::LoadingError),
-    #[error("no physical devices that support Vulkan also support computing")]
-    NoVulkanComputingDevices,
-    #[error("no physical devices that support Vulkan were found")]
-    NoVulkanDevices,
-    #[error(transparent)]
-    VulkanValidated(#[from] vulkano::Validated<vulkano::VulkanError>),
-    #[error(transparent)]
-    Vulkan(#[from] vulkano::VulkanError),
-    #[error("device of provided value was not the expected device")]
-    WrongDevice {
-        expected_device: Arc<Device>,
-        got_device: Arc<Device>,
-        /// (If applicable) the argument, field, etc whose passed value
-        offending_argument: Option<&'static str>,
-    },
-}
+// #[derive(Debug, Error)]
+// pub enum Error {
+//     #[error("device selector condition failed")]
+//     DeviceSelectorFailed,
+//     #[error(
+//         "{index} is not an index for a valid queue family for physical device {physical_device:?}"
+//     )]
+//     InvalidQueueFamilyIndexPhysicalDevice {
+//         index: u32,
+//         physical_device: Arc<PhysicalDevice>,
+//     },
+//     #[error("{index} is not an index for a valid queue family for vulkano device {device:?}")]
+//     InvalidQueueFamilyIndexDevice { index: u32, device: Arc<Device> },
+//     #[error(transparent)]
+//     LibraryLoading(#[from] vulkano::library::LoadingError),
+//     #[error("no physical devices that support Vulkan also support computing")]
+//     NoVulkanComputingDevices,
+//     #[error("no physical devices that support Vulkan were found")]
+//     NoVulkanDevices,
+//     #[error(transparent)]
+//     VulkanValidated(#[from] vulkano::Validated<vulkano::VulkanError>),
+//     #[error(transparent)]
+//     Vulkan(#[from] vulkano::VulkanError),
+//     #[error("device of provided value was not the expected device")]
+//     WrongDevice {
+//         expected_device: Arc<Device>,
+//         got_device: Arc<Device>,
+//         /// (If applicable) the argument, field, etc whose passed value
+//         offending_argument: Option<&'static str>,
+//     },
+// }
