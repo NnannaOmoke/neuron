e38a1d787d2e3725d70acf86dc23efed724e3a7c
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-06-26 12:49:40 -0700

    Add to Operations in compute.rs. Very WIP.

--- src/gpu_accel/compute.rs
+++ src/gpu_accel/compute.rs
@@ -1,5 +1,5 @@
 use super::context::GpuContext;
-use std::{ops::Deref, sync::Arc};
+use std::{ops::Deref, sync::{Arc, OnceLock}};
 
 pub struct ComputeContext<GpuContextPtr: Deref<Target = GpuContext>> {
     gpu_context: GpuContextPtr,
@@ -45,20 +45,73 @@ where
     }
 }
 
+pub struct WgpuPipelines {
+    dot_in_place: OnceLock<wgpu::ComputePipeline>,
+    dot_extern: OnceLock<wgpu::ComputePipeline>,
+}
+
 pub enum Operation {
+    CopyBufferToBuffer {
+        source: Arc<wgpu::Buffer>,
+        source_offset: wgpu::BufferAddress,
+        target: Arc<wgpu::Buffer>,
+        target_offset: wgpu::BufferAddress,
+        copy_len: wgpu::BufferAddress,
+    },
     DotInPlace {
         a_and_out: Arc<wgpu::Buffer>,
         b: Arc<wgpu::Buffer>,
+        wgpu_label: String,
     },
     DotExtern {
         a: Arc<wgpu::Buffer>,
         b: Arc<wgpu::Buffer>,
         output: Arc<wgpu::Buffer>,
+        wgpu_label: String,
     },
 }
 
 impl Operation {
     pub fn push_to_command_encoder(&self, command_encoder: &mut wgpu::CommandEncoder) {
-        todo!();
+        match self {
+            Self::CopyBufferToBuffer {
+                source,
+                source_offset,
+                target,
+                target_offset,
+                copy_len,
+            } => {
+                command_encoder.copy_buffer_to_buffer(
+                    &source,
+                    *source_offset,
+                    &target,
+                    *target_offset,
+                    *copy_len,
+                );
+            }
+            Self::DotInPlace {
+                a_and_out,
+                b,
+                wgpu_label,
+            } => {
+                let mut ce = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                    label: Some(&wgpu_label),
+                    timestamp_writes: None,
+                });
+                todo!();
+            }
+            Self::DotExtern {
+                a,
+                b,
+                output,
+                wgpu_label,
+            } => {
+                let mut ce = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                    label: Some(&wgpu_label),
+                    timestamp_writes: None,
+                });
+                todo!();
+            }
+        }
     }
 }
