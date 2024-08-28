a83956e2ceb41e8fc47975b4e3086e312feea173
Nnanna Omoke <omokennanna832@gmail.com>
2024-08-28 23:14:53 +0100

    Complete refactor of the regression/classification trees, next step will be to work on the cross validator

--- src/trees/classification_tree.rs
+++ src/trees/classification_tree.rs
@@ -6,9 +6,14 @@ use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
 
 use crate::{
     base_array::BaseDataset,
-    utils::model_selection::{TrainTestSplitStrategy, TrainTestSplitStrategyData},
+    utils::{
+        model_selection::{TrainTestSplitStrategy, TrainTestSplitStrategyData},
+        scaler::Scaler,
+        scaler::ScalerState,
+    },
 };
 
+#[derive(Debug, Clone, Default)]
 struct ClassificationTreeNode {
     left: Option<Box<ClassificationTreeNode>>,
     right: Option<Box<ClassificationTreeNode>>,
@@ -17,47 +22,33 @@ struct ClassificationTreeNode {
     value: u32,
     threshold: f64,
 }
-pub struct ClassificationTreeBuilder {
+
+#[derive(Debug, Clone, Default)]
+pub struct RawClassificationTree {
+    root: Option<Box<ClassificationTreeNode>>,
     min_samples: usize,
     max_depth: usize,
-    target_col: usize,
-    strategy: TrainTestSplitStrategy,
-    strategy_data: TrainTestSplitStrategyData<f64, u32>,
-    root: Option<Box<ClassificationTreeNode>>,
-}
-struct Split {
-    gain: f64,
-    threshold: f64,
-    feature_idx: usize,
-    left: Vec<usize>,
-    right: Vec<usize>,
-}
-impl Split {
-    fn new() -> Self {
-        Self {
-            gain: 0.0,
-            feature_idx: 0,
-            threshold: 0.0,
-            left: vec![],
-            right: vec![],
-        }
-    }
 }
 
-impl ClassificationTreeBuilder {
-    pub fn new() -> Self {
-        Self {
-            min_samples: 10,
-            max_depth: 10,
-            target_col: 0,
-            strategy: TrainTestSplitStrategy::None,
-            strategy_data: TrainTestSplitStrategyData::default(),
-            root: None,
-        }
+impl RawClassificationTree {
+    fn fit(&mut self, features: ArrayView2<f64>, labels: ArrayView1<u32>) {
+        self.root =
+            Some(self.build_internal(features, labels, Vec::from_iter(0..features.nrows()), 0));
     }
 
-    pub fn strategy(self, strategy: TrainTestSplitStrategy) -> Self {
-        Self { strategy, ..self }
+    fn predict(&self, data: ArrayView2<f64>) -> Array1<u32> {
+        let mut array = Array1::<u32>::default(data.nrows());
+        let mut idx = 0usize;
+        match &self.root {
+            None => array,
+            Some(node) => {
+                for row in data.rows() {
+                    array[idx] = node.predict(row);
+                    idx += 1;
+                }
+                array
+            }
+        }
     }
 
     fn split_data(
@@ -78,6 +69,7 @@ impl ClassificationTreeBuilder {
         }
         (left, right)
     }
+
     fn entropy(data: &[u32]) -> f64 {
         let mut entropy = 0.0;
         Self::for_each_unique(data, |val| {
@@ -87,6 +79,7 @@ impl ClassificationTreeBuilder {
         });
         entropy
     }
+
     fn information_gain(parent: &[u32], left: &[u32], right: &[u32]) -> f64 {
         let parent_entropy = Self::entropy(parent);
         let w_left = left.len() as f64 / parent.len() as f64;
@@ -96,6 +89,7 @@ impl ClassificationTreeBuilder {
         let wieghted_entropy = w_left * e_left + w_right * e_right;
         parent_entropy - wieghted_entropy
     }
+
     fn for_each_unique<T>(array: &[T], mut func: impl FnMut(T) -> ())
     where
         T: PartialEq + Copy,
@@ -108,6 +102,7 @@ impl ClassificationTreeBuilder {
             }
         }
     }
+
     fn column_from_index_list(
         dataset: ArrayView2<f64>,
         index_list: &[usize],
@@ -116,6 +111,7 @@ impl ClassificationTreeBuilder {
         let column = dataset.column(col_index);
         Self::pick_values(column, index_list)
     }
+
     fn pick_values<T>(array: ArrayView1<T>, index_list: &[usize]) -> Vec<T>
     where
         T: Copy,
@@ -126,6 +122,7 @@ impl ClassificationTreeBuilder {
         }
         out_column
     }
+
     fn best_split(
         &self,
         dataset: ArrayView2<f64>,
@@ -156,6 +153,7 @@ impl ClassificationTreeBuilder {
         }
         best_split
     }
+
     fn leaf_value(y: Vec<u32>) -> u32 {
         let mut highest_occurence = (0u32, 0usize); //(value, occurences)
         Self::for_each_unique(&y, |val| {
@@ -166,25 +164,22 @@ impl ClassificationTreeBuilder {
         });
         highest_occurence.0
     }
-    pub fn fit(&mut self, dataset: &BaseDataset, target: &str) {
-        self.target_col = dataset._get_string_index(target);
-        self.strategy_data =
-            TrainTestSplitStrategyData::<f64, u32>::new_c(dataset, self.target_col, self.strategy);
-        let n = self.strategy_data.get_train().0.nrows();
-        self.root = Some(self.build_internal(Vec::from_iter(0..n), 0));
-    }
+
     fn build_internal(
         &mut self,
+        features: ArrayView2<f64>,
+        labels: ArrayView1<u32>,
         index_list: Vec<usize>,
         current_depth: usize,
     ) -> Box<ClassificationTreeNode> {
-        let dataset = self.strategy_data.get_train();
-        let num_samples = dataset.0.nrows();
+        let num_samples = features.nrows();
         if num_samples >= self.min_samples && current_depth <= self.max_depth {
-            let best_split = self.best_split(dataset.0, dataset.1, &index_list);
+            let best_split = self.best_split(features, labels, &index_list);
             if best_split.gain != 0.0 {
-                let left = self.build_internal(best_split.left, current_depth + 1);
-                let right = self.build_internal(best_split.right, current_depth + 1);
+                let left =
+                    self.build_internal(features, labels, best_split.left, current_depth + 1);
+                let right =
+                    self.build_internal(features, labels, best_split.right, current_depth + 1);
                 return Box::new(ClassificationTreeNode {
                     left: Some(left),
                     right: Some(right),
@@ -195,7 +190,7 @@ impl ClassificationTreeBuilder {
                 });
             }
         }
-        let value = Self::leaf_value(Self::pick_values(dataset.1, &index_list));
+        let value = Self::leaf_value(Self::pick_values(labels, &index_list));
         Box::new(ClassificationTreeNode {
             left: None,
             right: None,
@@ -205,20 +200,103 @@ impl ClassificationTreeBuilder {
             value,
         })
     }
-    pub fn predict(&self, data: ArrayView2<f64>) -> Array1<u32> {
-        let mut array = Array1::<u32>::default(data.nrows());
-        let mut idx = 0usize;
-            match &self.root {
-            None => array,
-            Some(node) => {
-                for row in data.rows() {
-                    array[idx] = node.predict(row);
-                    idx += 1;
-                }
-                array
-            }
+}
+
+pub struct ClassificationTreeBuilder {
+    target_col: usize,
+    strategy: TrainTestSplitStrategy,
+    data: TrainTestSplitStrategyData<f64, u32>,
+    scaler: ScalerState,
+    tree: RawClassificationTree,
+}
+
+struct Split {
+    gain: f64,
+    threshold: f64,
+    feature_idx: usize,
+    left: Vec<usize>,
+    right: Vec<usize>,
+}
+impl Split {
+    fn new() -> Self {
+        Self {
+            gain: 0.0,
+            feature_idx: 0,
+            threshold: 0.0,
+            left: vec![],
+            right: vec![],
         }
     }
+}
+
+impl ClassificationTreeBuilder {
+    pub fn new() -> Self {
+        Self {
+            target_col: 0,
+            strategy: TrainTestSplitStrategy::None,
+            scaler: ScalerState::default(),
+            data: TrainTestSplitStrategyData::default(),
+            tree: RawClassificationTree::default(),
+        }
+    }
+
+    pub fn strategy(self, strategy: TrainTestSplitStrategy) -> Self {
+        Self { strategy, ..self }
+    }
+
+    pub fn scaler(self, scaler: ScalerState) -> Self {
+        Self { scaler, ..self }
+    }
+
+    pub fn min_samples(self, min_samples: usize) -> Self {
+        let prev = self.tree;
+        Self {
+            tree: RawClassificationTree {
+                min_samples,
+                ..prev
+            },
+            ..self
+        }
+    }
+
+    pub fn max_depth(self, max_depth: usize) -> Self {
+        let prev = self.tree;
+        Self {
+            tree: RawClassificationTree { max_depth, ..prev },
+            ..self
+        }
+    }
+
+    pub fn fit(&mut self, dataset: &BaseDataset, target: &str) {
+        self.target_col = dataset._get_string_index(target);
+        self.data =
+            TrainTestSplitStrategyData::<f64, u32>::new_c(dataset, self.target_col, self.strategy);
+
+        let mut scaler = Scaler::from(&self.scaler);
+        scaler.fit(self.data.get_train().0);
+        scaler.transform(&mut self.data.get_train_mut().0);
+        match self.strategy {
+            TrainTestSplitStrategy::TrainTest(_) => {
+                scaler.transform(&mut self.data.get_test_mut().0);
+            }
+            TrainTestSplitStrategy::TrainTestEval(_, _, _) => {
+                scaler.transform(&mut self.data.get_test_mut().0);
+                scaler.transform(&mut self.data.get_test_mut().0);
+            }
+            _ => {}
+        };
+        self.internal_fit();
+    }
+
+    fn internal_fit(&mut self) {
+        let (features, labels) = self.data.get_train();
+        self.tree.fit(features, labels);
+    }
+
+    pub fn predict(&self, data: ArrayView2<f64>) -> Array1<u32> {
+        self.tree.predict(data)
+    }
+
     pub fn evaluate<F>(&self, function: F) -> Vec<f64>
     //using a vec because user evaluation functions might return maybe one value or three
     //all the functions we plan to build in will only return one value, however
@@ -228,17 +306,17 @@ impl ClassificationTreeBuilder {
         let (features, ground_truth) = match self.strategy {
             TrainTestSplitStrategy::None => {
                 //get train data
-                self.strategy_data.get_train()
+                self.data.get_train()
             }
-            TrainTestSplitStrategy::TrainTest(_) => self.strategy_data.get_test(),
-            TrainTestSplitStrategy::TrainTestEval(_, _, _) => self.strategy_data.get_eval(),
+            TrainTestSplitStrategy::TrainTest(_) => self.data.get_test(),
+            TrainTestSplitStrategy::TrainTestEval(_, _, _) => self.data.get_eval(),
         };
         let preds = self.predict(features);
-            dbg!(&preds);
         function(ground_truth, preds.view())
     }
-    fn display(&self) {
-        self.root.as_ref().unwrap().display(0);
+
+    pub fn raw_mut(&mut self) -> &mut RawClassificationTree {
+        &mut self.tree
     }
 }
 impl ClassificationTreeNode {
@@ -255,14 +333,17 @@ impl ClassificationTreeNode {
     }
     fn display(&self, depth: usize) {
         if self.left.is_some() && self.right.is_some() {
-            println!("{} feature: {} gain: {} threshold: {}", "  ".repeat(depth),
+            println!(
+                "{} feature: {} gain: {} threshold: {}",
+                "  ".repeat(depth),
                 self.feature_idx,
                 self.gain,
-                self.threshold);
+                self.threshold
+            );
             println!("{} left:", "  ".repeat(depth));
-            self.left.as_ref().unwrap().display(depth+1);
+            self.left.as_ref().unwrap().display(depth + 1);
             println!("{} right:", "  ".repeat(depth));
-            self.right.as_ref().unwrap().display(depth+1);
+            self.right.as_ref().unwrap().display(depth + 1);
         } else {
             println!("{} value: {}", "  ".repeat(depth), self.value);
         }
--- src/trees/regression_tree.rs
+++ src/trees/regression_tree.rs
@@ -6,9 +6,14 @@ use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
 
 use crate::{
     base_array::BaseDataset,
-    utils::model_selection::{TrainTestSplitStrategy, TrainTestSplitStrategyData},
+    utils::{
+        model_selection::{TrainTestSplitStrategy, TrainTestSplitStrategyData},
+        scaler::Scaler,
+        scaler::ScalerState,
+    },
 };
 
+#[derive(Debug, Clone, Default)]
 struct RegressionTreeNode {
     left: Option<Box<RegressionTreeNode>>,
     right: Option<Box<RegressionTreeNode>>,
@@ -17,47 +22,34 @@ struct RegressionTreeNode {
     value: f64,
     threshold: f64,
 }
-pub struct RegressionTreeBuilder {
+
+#[derive(Debug, Clone, Default)]
+pub struct RawRegressionTree {
+    root: Option<Box<RegressionTreeNode>>,
     min_samples: usize,
     max_depth: usize,
-    target_col: usize,
-    strategy: TrainTestSplitStrategy,
-    strategy_data: TrainTestSplitStrategyData<f64, f64>,
-    root: Option<Box<RegressionTreeNode>>,
-}
-struct Split {
-    gain: f64,
-    threshold: f64,
-    feature_idx: usize,
-    left: Vec<usize>,
-    right: Vec<usize>,
-}
-impl Split {
-    fn new() -> Self {
-        Self {
-            gain: 0.0,
-            feature_idx: 0,
-            threshold: 0.0,
-            left: vec![],
-            right: vec![],
-        }
-    }
 }
 
-impl RegressionTreeBuilder {
-    pub fn new() -> Self {
-        Self {
-            min_samples: 10,
-            max_depth: 10,
-            target_col: 0,
-            strategy: TrainTestSplitStrategy::None,
-            strategy_data: TrainTestSplitStrategyData::default(),
-            root: None,
+impl RawRegressionTree {
+    fn fit(&mut self, features: ArrayView2<f64>, labels: ArrayView1<f64>) {
+        self.root =
+            Some(self.build_internal(features, labels, Vec::from_iter(0..features.nrows()), 0));
+    }
+    fn predict(&self, data: ArrayView2<f64>) -> Array1<f64> {
+        let mut array = Array1::<f64>::default(data.nrows());
+        let mut idx = 0usize;
+        match &self.root {
+            None => array,
+            Some(node) => {
+                for row in data.rows() {
+                    array[idx] = node.predict(row);
+                    idx += 1;
+                }
+                array
+            }
         }
     }
-    pub fn strategy(self, strategy: TrainTestSplitStrategy) -> Self {
-        Self { strategy, ..self }
-    }
+
     fn split_data(
         &self,
         dataset: ArrayView2<f64>,
@@ -76,6 +68,7 @@ impl RegressionTreeBuilder {
         }
         (left, right)
     }
+
     fn mse(data: &[f64]) -> f64 {
         let mut error = 0.0;
         let mut avg = 0.0;
@@ -89,6 +82,7 @@ impl RegressionTreeBuilder {
         error /= data.len() as f64;
         error
     }
+
     fn impurity_gain(parent: &[f64], left: &[f64], right: &[f64]) -> f64 {
         let parent_mse = Self::mse(parent);
         let mse_left = Self::mse(left);
@@ -97,6 +91,7 @@ impl RegressionTreeBuilder {
         let w_right = right.len() as f64 / parent.len() as f64;
         parent_mse - w_left * mse_left + w_right * mse_right
     }
+
     fn for_each_unique<T>(array: &[T], mut func: impl FnMut(T) -> ())
     where
         T: PartialEq + Copy,
@@ -109,6 +104,7 @@ impl RegressionTreeBuilder {
             }
         }
     }
+
     fn column_from_index_list(
         dataset: ArrayView2<f64>,
         index_list: &[usize],
@@ -117,6 +113,7 @@ impl RegressionTreeBuilder {
         let column = dataset.column(col_index);
         Self::pick_values(column, index_list)
     }
+
     fn pick_values<T>(array: ArrayView1<T>, index_list: &[usize]) -> Vec<T>
     where
         T: Copy,
@@ -127,6 +124,7 @@ impl RegressionTreeBuilder {
         }
         out_column
     }
+
     fn best_split(
         &self,
         dataset: ArrayView2<f64>,
@@ -157,6 +155,7 @@ impl RegressionTreeBuilder {
         }
         best_split
     }
+
     fn leaf_value(y: Vec<f64>) -> f64 {
         let mut val = 0.0;
         for elem in &y {
@@ -164,25 +163,22 @@ impl RegressionTreeBuilder {
         }
         val / y.len() as f64
     }
-    pub fn fit(&mut self, dataset: &BaseDataset, target: &str) {
-        self.target_col = dataset._get_string_index(target);
-        self.strategy_data =
-            TrainTestSplitStrategyData::<f64, f64>::new_r(dataset, self.target_col, self.strategy);
-        let n = self.strategy_data.get_train().0.nrows();
-        self.root = Some(self.build_internal(Vec::from_iter(0..n), 0));
-    }
+
     fn build_internal(
         &mut self,
+        features: ArrayView2<f64>,
+        labels: ArrayView1<f64>,
         index_list: Vec<usize>,
         current_depth: usize,
     ) -> Box<RegressionTreeNode> {
-        let dataset = self.strategy_data.get_train();
-        let num_samples = dataset.0.nrows();
+        let num_samples = features.nrows();
         if num_samples >= self.min_samples && current_depth <= self.max_depth {
-            let best_split = self.best_split(dataset.0, dataset.1, &index_list);
+            let best_split = self.best_split(features, labels, &index_list);
             if best_split.gain != 0.0 {
-                let left = self.build_internal(best_split.left, current_depth + 1);
-                let right = self.build_internal(best_split.right, current_depth + 1);
+                let left =
+                    self.build_internal(features, labels, best_split.left, current_depth + 1);
+                let right =
+                    self.build_internal(features, labels, best_split.right, current_depth + 1);
                 return Box::new(RegressionTreeNode {
                     left: Some(left),
                     right: Some(right),
@@ -193,7 +189,7 @@ impl RegressionTreeBuilder {
                 });
             }
         }
-        let value = Self::leaf_value(Self::pick_values(dataset.1, &index_list));
+        let value = Self::leaf_value(Self::pick_values(labels, &index_list));
         Box::new(RegressionTreeNode {
             left: None,
             right: None,
@@ -203,19 +199,99 @@ impl RegressionTreeBuilder {
             value,
         })
     }
-    pub fn predict(&self, data: ArrayView2<f64>) -> Array1<f64> {
-        let mut array = Array1::<f64>::default(data.nrows());
-        let mut idx = 0usize;
-        match &self.root {
-            None => array,
-            Some(node) => {
-                for row in data.rows() {
-                    array[idx] = node.predict(row);
-                    idx += 1;
-                }
-                array
-            }
+}
+
+pub struct RegressionTreeBuilder {
+    target_col: usize,
+    strategy: TrainTestSplitStrategy,
+    data: TrainTestSplitStrategyData<f64, f64>,
+    scaler: ScalerState,
+    tree: RawRegressionTree,
+}
+struct Split {
+    gain: f64,
+    threshold: f64,
+    feature_idx: usize,
+    left: Vec<usize>,
+    right: Vec<usize>,
+}
+impl Split {
+    fn new() -> Self {
+        Self {
+            gain: 0.0,
+            feature_idx: 0,
+            threshold: 0.0,
+            left: vec![],
+            right: vec![],
         }
+    }
+}
+
+impl RegressionTreeBuilder {
+    pub fn new() -> Self {
+        Self {
+            target_col: 0,
+            strategy: TrainTestSplitStrategy::None,
+            data: TrainTestSplitStrategyData::default(),
+            scaler: ScalerState::default(),
+            tree: RawRegressionTree::default(),
+        }
+    }
+
+    pub fn strategy(self, strategy: TrainTestSplitStrategy) -> Self {
+        Self { strategy, ..self }
+    }
+
+    pub fn scaler(self, scaler: ScalerState) -> Self {
+        Self { scaler, ..self }
+    }
+
+    pub fn min_samples(self, min_samples: usize) -> Self {
+        let prev = self.tree;
+        Self {
+            tree: RawRegressionTree {
+                min_samples,
+                ..prev
+            },
+            ..self
+        }
+    }
+
+    pub fn max_depth(self, max_depth: usize) -> Self {
+        let prev = self.tree;
+        Self {
+            tree: RawRegressionTree { max_depth, ..prev },
+            ..self
+        }
+    }
+
+    pub fn fit(&mut self, dataset: &BaseDataset, target: &str) {
+        self.target_col = dataset._get_string_index(target);
+        self.data =
+            TrainTestSplitStrategyData::<f64, f64>::new_r(dataset, self.target_col, self.strategy);
+        let mut scaler = Scaler::from(&self.scaler);
+        scaler.fit(self.data.get_train().0);
+        scaler.transform(&mut self.data.get_train_mut().0);
+        match self.strategy {
+            TrainTestSplitStrategy::TrainTest(_) => {
+                scaler.transform(&mut self.data.get_test_mut().0);
+            }
+            TrainTestSplitStrategy::TrainTestEval(_, _, _) => {
+                scaler.transform(&mut self.data.get_test_mut().0);
+                scaler.transform(&mut self.data.get_test_mut().0);
+            }
+            _ => {}
+        };
+        self.internal_fit();
+    }
+
+    pub fn internal_fit(&mut self) {
+        let (features, labels) = self.data.get_train();
+        self.tree.fit(features, labels);
+    }
+
+    pub fn predict(&self, data: ArrayView2<f64>) -> Array1<f64> {
+        self.tree.predict(data)
     }
 
     pub fn evaluate<F>(&self, function: F) -> Vec<f64>
@@ -227,16 +303,13 @@ impl RegressionTreeBuilder {
         let (features, ground_truth) = match self.strategy {
             TrainTestSplitStrategy::None => {
                 //get train data
-                self.strategy_data.get_train()
+                self.data.get_train()
             }
-            TrainTestSplitStrategy::TrainTest(_) => self.strategy_data.get_test(),
-            TrainTestSplitStrategy::TrainTestEval(_, _, _) => self.strategy_data.get_eval(),
+            TrainTestSplitStrategy::TrainTest(_) => self.data.get_test(),
+            TrainTestSplitStrategy::TrainTestEval(_, _, _) => self.data.get_eval(),
         };
         let preds = self.predict(features);
         function(ground_truth, preds.view())
-    }
-    fn display(&self) {
-        self.root.as_ref().unwrap().display(0);
     }
 }
 impl RegressionTreeNode {
@@ -253,14 +326,17 @@ impl RegressionTreeNode {
     }
     fn display(&self, depth: usize) {
         if self.left.is_some() && self.right.is_some() {
-            println!("{} feature: {} gain: {} threshold: {}", "  ".repeat(depth),
+            println!(
+                "{} feature: {} gain: {} threshold: {}",
+                "  ".repeat(depth),
                 self.feature_idx,
                 self.gain,
-                self.threshold);
+                self.threshold
+            );
             println!("{} left:", "  ".repeat(depth));
-            self.left.as_ref().unwrap().display(depth+1);
+            self.left.as_ref().unwrap().display(depth + 1);
             println!("{} right:", "  ".repeat(depth));
-            self.right.as_ref().unwrap().display(depth+1);
+            self.right.as_ref().unwrap().display(depth + 1);
         } else {
             println!("{} value: {}", "  ".repeat(depth), self.value);
         }
