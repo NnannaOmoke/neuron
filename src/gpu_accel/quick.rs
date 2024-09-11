471614ab912e4be95b6205f59558d9c09068985a
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-09-10 22:02:55 -0700

    Primitive implementation of matmul32_in_place_lhs.

--- src/gpu_accel/quick.rs
+++ src/gpu_accel/quick.rs
@@ -1,3 +1,5 @@
+//! Todo: `matmul32_in_place_rhs`?
+
 use crate::gpu_accel::utils::{create_loaded_buffer, create_loaded_buffer_from_mut};
 
 use super::{shaders::init_dot_in_place, utils};
@@ -7,24 +9,7 @@ use thiserror::Error;
 use tokio::sync::Notify;
 use wgpu::util::DeviceExt;
 
-/// Preforms matrix multiplication of f32s into a mutable output array view.
-pub async fn matmul32_extern(
-    lhs: ArrayView2<'_, f32>,
-    rhs: ArrayView2<'_, f32>,
-    mut out: ArrayViewMut2<'_, f32>,
-) -> Result<(), utils::Error> {
-    assert_eq!(
-        lhs.dim().1,
-        rhs.dim().0,
-        "col count of lhs must == row count of rhs"
-    );
-    let out_dim = out.dim();
-    assert_eq!(
-        out_dim,
-        (lhs.dim().0, rhs.dim().1),
-        "out dims must be row_count(lhs) * col_count(rhs)"
-    );
-
+async fn get_wgpu_device() -> (wgpu::Device, wgpu::Queue) {
     let instance = wgpu::Instance::default();
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
@@ -34,7 +19,7 @@ pub async fn matmul32_extern(
         })
         .await
         .expect("could not get wgpu adapter");
-    let (device, queue) = adapter
+    adapter
         .request_device(
             &wgpu::DeviceDescriptor {
                 label: Some("neuron quick matmul32 device"),
@@ -44,30 +29,16 @@ pub async fn matmul32_extern(
             None,
         )
         .await
-        .expect("could not get device from wgpu adapter");
+        .expect("could not get device from wgpu adapter")
+}
 
-    // See docs for `utils::create_loaded_buffer`.
-    let lhs_buffer = create_loaded_buffer(&device, &lhs, "`matmul32` `lhs`").await?;
-    let rhs_buffer = create_loaded_buffer(&device, &rhs, "`matmul33` `rhs`").await?;
-    let dims_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("neuron matmul32 input dims buffer"),
-        contents: bytemuck::bytes_of(&[lhs.dim().0, lhs.dim().1, rhs.dim().0, rhs.dim().1]),
-        usage: wgpu::BufferUsages::UNIFORM,
-    });
-    let output_buffer_size = (out_dim.0 * out_dim.1 * size_of::<f32>()) as wgpu::BufferAddress;
-    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: Some("neuron matmul32 output buffer"),
-        size: output_buffer_size,
-        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-        mapped_at_creation: false,
-    });
-    let output_staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-        label: Some("neuron matmul32 output staging buffer"),
-        size: output_buffer_size,
-        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
-        mapped_at_creation: false,
-    });
-
+fn matmul32_create_bind_group_and_pipeline(
+    device: &wgpu::Device,
+    lhs_buf: &wgpu::Buffer,
+    rhs_buf: &wgpu::Buffer,
+    dims_buf: &wgpu::Buffer,
+    output_buf: &wgpu::Buffer,
+) -> (wgpu::BindGroup, wgpu::ComputePipeline) {
     let shader_module = init_dot_in_place(&device);
 
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -121,19 +92,19 @@ pub async fn matmul32_extern(
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource: lhs_buffer.as_entire_binding(),
+                resource: lhs_buf.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 1,
-                resource: rhs_buffer.as_entire_binding(),
+                resource: rhs_buf.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 2,
-                resource: dims_buffer.as_entire_binding(),
+                resource: dims_buf.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 3,
-                resource: output_buffer.as_entire_binding(),
+                resource: output_buf.as_entire_binding(),
             },
         ],
     });
@@ -151,12 +122,65 @@ pub async fn matmul32_extern(
         compilation_options: wgpu::PipelineCompilationOptions::default(),
     });
 
+    (bind_group, compute_pipeline)
+}
+
+/// Preforms matrix multiplication of f32s into a mutable output array view.
+pub async fn matmul32_extern(
+    lhs: ArrayView2<'_, f32>,
+    rhs: ArrayView2<'_, f32>,
+    mut out: ArrayViewMut2<'_, f32>,
+) -> Result<(), utils::Error> {
+    assert_eq!(
+        lhs.dim().1,
+        rhs.dim().0,
+        "col count of lhs must == row count of rhs"
+    );
+    let out_dim = out.dim();
+    assert_eq!(
+        out_dim,
+        (lhs.dim().0, rhs.dim().1),
+        "out dims must be row_count(lhs) * col_count(rhs)"
+    );
+
+    let (device, queue) = get_wgpu_device().await;
+
+    // See docs for `utils::create_loaded_buffer`.
+    let lhs_buffer = create_loaded_buffer(&device, &lhs, "`matmul32_extern` `lhs`").await?;
+    let rhs_buffer = create_loaded_buffer(&device, &rhs, "`matmul33_extern` `rhs`").await?;
+    let dims_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("neuron matmul32 input dims buffer"),
+        contents: bytemuck::bytes_of(&[lhs.dim().0, lhs.dim().1, rhs.dim().0, rhs.dim().1]),
+        usage: wgpu::BufferUsages::UNIFORM,
+    });
+    let output_buffer_size = (out_dim.0 * out_dim.1 * size_of::<f32>()) as wgpu::BufferAddress;
+    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("neuron matmul32 output buffer"),
+        size: output_buffer_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+    let output_staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("neuron matmul32 output staging buffer"),
+        size: output_buffer_size,
+        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    let (bind_group, compute_pipeline) = matmul32_create_bind_group_and_pipeline(
+        &device,
+        &lhs_buffer,
+        &rhs_buffer,
+        &dims_buffer,
+        &output_buffer,
+    );
+
     let mut command_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-        label: Some("neuron quick matmul32 command encoder"),
+        label: Some("neuron quick matmul32_extern command encoder"),
     });
     {
         let mut compute_pass = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-            label: Some("neuron quick matmul32 compute pass"),
+            label: Some("neuron quick matmul32_extern compute pass"),
             timestamp_writes: None,
         });
         compute_pass.set_pipeline(&compute_pipeline);
@@ -190,6 +214,99 @@ pub async fn matmul32_extern(
         mut_target_slice.copy_from_slice(bytemuck::cast_slice(&mapped_range));
     } else {
         for (i, e) in out.iter_mut().enumerate() {
+            *e = *bytemuck::from_bytes(&mapped_range[i..i + 4]);
+        }
+    }
+
+    Ok(())
+}
+
+/// Same as matmul32_extern but it outputs into the lhs CPU buffer.
+///
+/// Note that this has the same GPU memory footprint, it just has the benefit of
+/// sometimes letting you get away with skipping an extra cpu allocation, especially
+/// with same-dim matrices.
+pub async fn matmul32_in_place_lhs(
+    mut lhs: ArrayViewMut2<'_, f32>,
+    rhs: ArrayView2<'_, f32>,
+) -> Result<(), utils::Error> {
+    let lhs_dim = lhs.dim();
+    let rhs_dim = rhs.dim();
+    assert_eq!(lhs_dim.1, rhs_dim.0, "lhs.cols must == rhs.rows");
+    assert_eq!(lhs_dim.1, rhs_dim.1, "lhs.cols must == rhs.cols");
+
+    let (device, queue) = get_wgpu_device().await;
+
+    // See docs for `utils::create_loaded_buffer`.
+    let lhs_buf =
+        create_loaded_buffer_from_mut(&device, &lhs, "`matmul32_in_place_lhs` `lhs`").await?;
+    let rhs_buf = create_loaded_buffer(&device, &rhs, "`matmul33_in_place_lhs` `rhs`").await?;
+    let dims_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("neuron matmul32 input dims buffer"),
+        contents: bytemuck::bytes_of(&[lhs_dim.0, lhs_dim.1, rhs_dim.0, rhs_dim.1]),
+        usage: wgpu::BufferUsages::UNIFORM,
+    });
+    let output_buffer_size = (lhs_dim.0 * lhs_dim.1 * size_of::<f32>()) as wgpu::BufferAddress;
+    let output_buf = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("neuron matmul32 output buffer"),
+        size: output_buffer_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+    let output_staging_buf = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("neuron matmul32 output staging buffer"),
+        size: output_buffer_size,
+        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    let (bind_group, compute_pipeline) = matmul32_create_bind_group_and_pipeline(
+        &device,
+        &lhs_buf,
+        &rhs_buf,
+        &dims_buf,
+        &output_buf,
+    );
+
+    let mut command_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("neuron quick matmul32_extern command encoder"),
+    });
+    {
+        let mut compute_pass = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some("neuron quick matmul32_extern compute pass"),
+            timestamp_writes: None,
+        });
+        compute_pass.set_pipeline(&compute_pipeline);
+        compute_pass.set_bind_group(0, &bind_group, &[]);
+        compute_pass.dispatch_workgroups(lhs_dim.0 as u32, lhs_dim.1 as u32, 0);
+    }
+    command_encoder.copy_buffer_to_buffer(
+        &output_buf,
+        0,
+        &output_staging_buf,
+        0,
+        output_buffer_size as wgpu::BufferAddress,
+    );
+    let submission_index = queue.submit([command_encoder.finish()]);
+
+    let output_slice = output_staging_buf.slice(..);
+
+    let (sender, receiver) = tokio::sync::oneshot::channel();
+    output_slice.map_async(wgpu::MapMode::Read, move |r| {
+        sender
+            .send(r)
+            .expect("failed to send buffer mapping result through channel")
+    });
+    device
+        .poll(wgpu::Maintain::WaitForSubmissionIndex(submission_index))
+        .panic_on_timeout();
+    receiver.await??;
+
+    let mapped_range = output_slice.get_mapped_range();
+    if let Some(mut_target_slice) = lhs.as_slice_mut() {
+        mut_target_slice.copy_from_slice(bytemuck::cast_slice(&mapped_range));
+    } else {
+        for (i, e) in lhs.iter_mut().enumerate() {
             *e = *bytemuck::from_bytes(&mapped_range[i..i + 4]);
         }
     }
